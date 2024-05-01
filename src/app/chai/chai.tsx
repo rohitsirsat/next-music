0df82810ac49318cb596d8d83d493b0e1b5937a6
rohit <rohitsirsat855@gmail.com>
2024-05-01 14:29:55 +0530

    music school app

--- src/app/chai/chai.tsx
+++
@@ -1,3 +0,0 @@
-export default function ChaiPage() {
-  return <h1>chai page</h1>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChoose from "@/components/WhyChoose";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-center text-3xl">chai aur code</h1>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChoose />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
+  );
 }
